da5e1cf2443873e4bc21f74ccf60318a61fa784c
aprodan <prodanalexandru580@gmail.com>
2023-02-01 22:05:24 +0200

    start working on the web api side

---
+++ src/web-api/CarsIsland.API/AuthorizationPolicies/ScopeRequirement.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace CarsIsland.API.AuthorizationPolicies;
+
+public class ScopesRequirement : IAuthorizationRequirement
+{
+    public readonly string ScopeName;
+    public ScopesRequirement(string scopeName)
+    {
+        ScopeName = scopeName;
+    }
+}
+
