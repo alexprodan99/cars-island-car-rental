b957facc9f6c5eeb109dc663807d5bb8df4159b9
aprodan <prodanalexandru580@gmail.com>
2023-02-02 22:34:01 +0200

    depency injection set authorization and policy

--- src/web-api/CarsIsland.API/Program.cs
+++ src/web-api/CarsIsland.API/Program.cs
@@ -1,3 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using CarsIsland.API.AuthorizationPolicies;
+using CarsIsland.API.Core.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +27,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAuthenticationWithAuthorizationSupport(builder.Configuration);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AccessAsUser", policy => policy.Requirements.Add(new ScopesRequirement("access_as_user")));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
